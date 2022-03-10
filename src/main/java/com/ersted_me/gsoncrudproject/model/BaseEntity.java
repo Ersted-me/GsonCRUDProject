dcd02018232943283dee2cc08e16f8d29a35bc76
Bogdan Rudko <rydkoc@icloud.com>
2022-03-10 13:54:32 +0300

    Initial commit

---
+++ src/main/java/com/ersted_me/gsoncrudproject/model/BaseEntity.java
@@ -0,0 +1,13 @@
+package com.ersted_me.gsoncrudproject.model;
+
+public class BaseEntity {
+    private Long Id;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+}
---
+++ src/main/java/com/ersted_me/gsoncrudproject/model/NamedEntity.java
@@ -0,0 +1,17 @@
+package com.ersted_me.gsoncrudproject.model;
+
+public class NamedEntity extends BaseEntity {
+    private String name;
+
+    public NamedEntity(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
