dd9330a0b70d7008d24dc2894d71d269ae92d674
Bogdan Rudko <rydkoc@icloud.com>
2022-03-12 13:54:39 +0300

    add Specialty layers.

---
+++ src/main/java/com/ersted_me/gsoncrudproject/repository/SpecialtyRepository.java
@@ -0,0 +1,6 @@
+package com.ersted_me.gsoncrudproject.repository;
+
+import com.ersted_me.gsoncrudproject.model.Specialty;
+
+public interface SpecialtyRepository extends GenericRepository<Specialty, Long>{
+}
